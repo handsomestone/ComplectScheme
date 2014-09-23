8849c5bb16e4bc258da10de832add5ee1511704d
jolud <jolud@microsoft.com>
2014-09-22 22:41:52 -0700

    Implemented recursive parser for heterogeneous types. Identified basic Scheme grammar, string literals, symbols, and lists.

--- ComplectScheme/Parser.fs
+++ ComplectScheme/Parser.fs
@@ -1,17 +1,44 @@
 ﻿module Parser
     open FParsec
 
-    type UserState = unit  // TODO -- may want to pass some useful state at some point, but required for value restriction
-    type Parser<'t> = Parser<'t, UserState>
+    type Identifier =
+        | Symbol of string
+        | Char of char
+        | Int of int
+        | List of Identifier list
+        | String of string
 
+    // Characters and string functions
     let reservedChars = Set.ofList [ '('; ')'; '\'' ]
     let isPunc c = System.Char.IsPunctuation(c)
     let isSymbol c = System.Char.IsSymbol(c)
     let isReserved c = reservedChars |> Set.contains c
-
+    let validStringContents c = c <> '"'
     let validChars c = (isLetter c || isDigit c || isPunc c || isSymbol c) && not (isReserved c)
 
-    let str s = pstring s
+    // Convenience functions
+    let str s = pstring s    
+    let listOf pElement f =
+        between (str "(") (str ")") (spaces >>. sepBy pElement spaces1 |>> f)
 
-    let parseList : Parser<_> =
-        between (str "(") (str ")") (sepBy (many1Satisfy validChars) spaces1)
+    // Forward declaration of parser, needed for recursive parser
+    let idValue, idValueRef = createParserForwardedToRef<Identifier, unit>()
+
+    // ( ... )
+    let list =
+        listOf idValue Identifier.List
+
+    // non-quoted string
+    let symbol : Parser<Identifier, unit> =
+        (many1Satisfy validChars) |>> Identifier.Symbol
+
+    // quoted string
+    let stringLiteral =
+        // TODO -- implement escape characters
+        between (str "\"") (str "\"") (manySatisfy validStringContents) |>> Identifier.String
+
+    // Defines the recursive parser to be a choice over the existing parsers
+    do idValueRef := choice [ stringLiteral; list; symbol ]
+
+    // Top-level form should be a list
+    let parse = list
