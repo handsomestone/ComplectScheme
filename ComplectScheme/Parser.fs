4855495bae4254ff3ecb83478ee0649477b4eae7
jolud <jolud@microsoft.com>
2014-09-22 21:36:08 -0700

    Implement initial list parser.

---
+++ ComplectScheme/Parser.fs
@@ -0,0 +1,17 @@
+﻿module Parser
+    open FParsec
+
+    type UserState = unit  // TODO -- may want to pass some useful state at some point, but required for value restriction
+    type Parser<'t> = Parser<'t, UserState>
+
+    let reservedChars = Set.ofList [ '('; ')'; '\'' ]
+    let isPunc c = System.Char.IsPunctuation(c)
+    let isSymbol c = System.Char.IsSymbol(c)
+    let isReserved c = reservedChars |> Set.contains c
+
+    let validChars c = (isLetter c || isDigit c || isPunc c || isSymbol c) && not (isReserved c)
+
+    let str s = pstring s
+
+    let parseList : Parser<_> =
+        between (str "(") (str ")") (sepBy (many1Satisfy validChars) spaces1)
---
+++ ComplectSchemeUnitTests/ParserTests.fs
@@ -0,0 +1,40 @@
+﻿namespace ComplectScheme.UnitTests
+
+open Microsoft.VisualStudio.TestTools.UnitTesting
+open FsUnit
+
+open FParsec
+open Parser
+
+[<TestClass>]
+type ParserTests() =
+    
+    let test parser input =
+        let r = run parser input
+        match r with 
+            | ParserResult.Success(x, _, _) -> x
+            | ParserResult.Failure(x, _, _) -> failwith x
+
+    [<TestMethod>]
+    member this.``1 item list``() =
+        let v = "(one)"
+
+        test Parser.parseList v |> should equal [ "one" ]
+
+    [<TestMethod>]
+    member this.``3 item list``() =
+        let v = "(one two three)"
+
+        test Parser.parseList v |> should equal [ "one"; "two"; "three" ]
+    
+    [<TestMethod>]
+    member this.``+ symbol``() =
+        let v = "(+ 1 2)"
+
+        test Parser.parseList v |> should equal [ "+"; "1"; "2" ]
+    
+//    [<TestMethod>]
+//    member this.``float list``() =
+//        let v = "(1.0)"
+//
+//        test Parser.parseList v |> should equal 1.0
